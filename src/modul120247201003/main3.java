bf876c040bef7752732dd3d9b24392e74147f66b
Nasrul809 <hadinasrul809@gmail.com>
2025-03-24 16:36:59 +0700

    perubahan1

---
+++ src/modul120247201003/main3.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul120247201003;
+
+public class main3 {
+     public static void main(String[] args){
+        String[][] data = {
+        {"ABDUL","Kediri","085646668991"},
+        {"KUSNO","Trenggalek","085646668992"},
+        {"PONIRAN","Bojonegoro","085646668999"}
+        };
+         System.out.print("Nama\t");
+         System.out.print("  Alamat\t");
+         System.out.print(" Telepon\t");
+         System.out.println("");
+         String nama ="";
+         
+         for (int a =0; a < data.length; a++){
+             System.out.print(String.format("%-10s", data[a][0]));
+             System.out.print(String.format("%-15s", data[a][1]));
+             System.out.print(String.format("%-10s", data[a][2]));
+             System.out.println("");
+         }
+    }
+}
